c363102fdfdb800112048513eaa1c207358ef170
aleksey.fosler <aleksey.fosler@gmail.com>
2016-07-10 23:43:03 +0600

    tabs - done

---
+++ app/src/main/java/com/example/aleks/forwork/FreeFragment.java
@@ -0,0 +1,31 @@
+package com.example.aleks.forwork;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by aleks on 10.07.2016.
+ */
+public class FreeFragment extends Fragment {
+
+    public FreeFragment () {
+        // empty
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_free, container, false);
+    }
+
+}
+
